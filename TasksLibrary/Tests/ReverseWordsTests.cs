3aa9e32a6f59a0564365d5c51808c00733265560
Daniil <anatoliikljukin@mail.ru>
2019-06-25 18:02:12 +0400

    Create ReverseWordsTests.cs
    Added tests for 5th task.

---
+++ TasksLibrary/Tests/ReverseWordsTests.cs
@@ -0,0 +1,22 @@
+﻿using TasksLibrary;
+using NUnit.Framework;
+using MSUnit = Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Tests
+{
+    [TestFixture]
+    [MSUnit.TestClass]
+    class ReverseWordsTests
+    {
+        [TestCase("The greatest victory is that which requires no battle",
+            ExpectedResult = "battle no requires which that is victory greatest The")]
+        [TestCase("one two three four five six seven eight nine ten",
+            ExpectedResult = "ten nine eight seven six five four three two one")]
+        [TestCase("word", ExpectedResult = "word")]
+        [TestCase("A B C D E F G", ExpectedResult = "G F E D C B A")]
+        public string CheckReverseWords(string text)
+        {
+            return Reverser.ReverseWords(text);
+        }
+    }
+}
