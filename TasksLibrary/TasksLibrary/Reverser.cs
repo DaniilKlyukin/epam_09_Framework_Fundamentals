43cc6df4ab5263c699f1948cdc8782246565545d
Daniil <anatoliikljukin@mail.ru>
2019-06-25 18:01:36 +0400

    Create Reverser.cs
    Solved 5th task (ReverseWords method).

---
+++ TasksLibrary/TasksLibrary/Reverser.cs
@@ -0,0 +1,13 @@
+﻿namespace TasksLibrary
+{
+    using System.Linq;
+
+    public class Reverser
+    {
+        public static string ReverseWords(string text)
+        {
+            return
+                string.Join(" ", text.Split(' ', '\t', '\n').Reverse());
+        }
+    }
+}
