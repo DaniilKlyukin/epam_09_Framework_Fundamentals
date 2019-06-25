fc932d900353fa9a0f2537ef1535c7253c44c017
Daniil <anatoliikljukin@mail.ru>
2019-06-25 15:43:50 +0400

    Create StringWorker.cs
    Solved 2nd task (created ToTitleCase method).

---
+++ TasksLibrary/TasksLibrary/StringWorker.cs
@@ -0,0 +1,35 @@
+﻿namespace TasksLibrary
+{
+    using System.Linq;
+    using System.Text;
+
+    public class StringWorker
+    {
+        public static string ToTitleCase(string text, string exceptionString = "")
+        {
+            var excWords = exceptionString.ToLower().Split(' ');
+            var textWords = text.ToLower().Split(' ');
+
+            var sBuilder = new StringBuilder(CapitalizeWord(textWords.First()) + " ");
+
+            for (int i = 1; i < textWords.Length; i++)
+            {
+                if (!excWords.Contains(textWords[i]))
+                    sBuilder.Append(CapitalizeWord(textWords[i]));
+                else
+                    sBuilder.Append(textWords[i]);
+
+                sBuilder.Append(" ");
+            }
+
+            sBuilder.Remove(sBuilder.Length - 1, 1);
+
+            return sBuilder.ToString();
+        }
+
+        private static string CapitalizeWord(string word)
+        {
+            return word.Substring(0, 1).ToUpper() + word.Substring(1, word.Length - 1);
+        }
+    }
+}
