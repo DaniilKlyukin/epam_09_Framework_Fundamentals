b51f98b9e401c804a565d8bd601ef609bbcec0ab
Daniil <anatoliikljukin@mail.ru>
2019-06-25 15:45:06 +0400

    Create UniqOrderer.cs
    Solved 4th task (added UniqueInOrder generic method)

---
+++ TasksLibrary/TasksLibrary/UniqOrderer.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TasksLibrary
+{
+    public class UniqOrderer
+    {
+        public static IEnumerable<T> UniqueInOrder<T>(IEnumerable<T> collection)
+        {
+            var enumerator = collection.GetEnumerator();
+
+            var stack = new Stack<T>();
+
+            while (enumerator.MoveNext())
+            {
+                if (!stack.Contains(enumerator.Current))
+                {
+                    if (stack.Count != 0)
+                        stack.Pop();
+                    yield return enumerator.Current;
+                    stack.Push(enumerator.Current);
+                }
+            }
+        }
+    }
+}
