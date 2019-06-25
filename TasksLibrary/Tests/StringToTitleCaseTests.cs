46fe2496e9c465738abfa61781066699c1faa358
Daniil <anatoliikljukin@mail.ru>
2019-06-25 15:45:29 +0400

    Create StringToTitleCaseTests.cs
    Added tests for 2nd task.

---
+++ TasksLibrary/Tests/StringToTitleCaseTests.cs
@@ -0,0 +1,21 @@
+﻿using TasksLibrary;
+using NUnit.Framework;
+using MSUnit = Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Tests
+{
+    [TestFixture]
+    [MSUnit.TestClass]
+    class StringToTitleCaseTests
+    {
+        [TestCase("a clash of KINGS", "a an the of", ExpectedResult = "A Clash of Kings")]
+        [TestCase("THE WIND IN THE WILLOWS", "The In", ExpectedResult = "The Wind in the Willows")]
+        [TestCase("the quick brown fox", ExpectedResult = "The Quick Brown Fox")]
+        [TestCase("sO sTrANGe leTTer tO TiTLe Case", "so to", ExpectedResult = "So Strange Letter to Title Case")]
+        [TestCase("Hi", "hi", ExpectedResult = "Hi")]
+        public string CheckToTitleCase(string text, string exceptionWords = "")
+        {
+            return StringWorker.ToTitleCase(text, exceptionWords);
+        }
+    }
+}
